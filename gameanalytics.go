882e0ed7f98eaf314eacbcf89b868b29e4017ca6
Luiz Branco <me@luizbranco.com>
2015-08-18 13:45:13 -0400

    Adapt chips_purchase event

--- gameanalytics.go
+++ gameanalytics.go
@@ -13,6 +13,13 @@ type sessionEndProperties struct {
 	Length int `json:"session_length"`
 }
 
+type chipsPurchaseProperties struct {
+	Amount   float64
+	Type     string
+	Provider string
+	Number   uint `json:"transaction_count"`
+}
+
 // GameAnalytics implements the adapter interface. It translates the events
 // received and forwards them to the GameAnalytics SDK server.
 type GameAnalytics struct {
@@ -52,7 +59,7 @@ func (a *GameAnalytics) listen(server *ga.Server) {
 			}
 			ts, err := a.getTimeDiff(e.Timestamp, server.TimestampOffset)
 			if err != nil {
-				log.Printf("[WARN] Event timestamp parsing failed %s (%e)", err, e)
+				log.Printf("[WARN] Event timestamp parsing failed %s (%v)", err, e)
 				continue
 			}
 			shared := &ga.DefaultAnnotations{
@@ -71,11 +78,27 @@ func (a *GameAnalytics) listen(server *ga.Server) {
 				s := &sessionEndProperties{}
 				err := json.Unmarshal(e.Properties, s)
 				if err != nil {
-					log.Printf("[WARN] JSON conversion failed %s", err)
+					log.Printf("[WARN] JSON conversion failed %s (%q)", err,
+						&e.Properties)
 					continue
 				}
 				ended.Length = s.Length
 				server.SendEvent(ended)
+			case "chips_purchase":
+				business := ga.NewBusinessEvent(shared)
+				c := &chipsPurchaseProperties{}
+				err := json.Unmarshal(e.Properties, c)
+				if err != nil {
+					log.Printf("[WARN] JSON conversion failed %s (%q)", err,
+						&e.Properties)
+					continue
+				}
+				business.EventID = fmt.Sprintf("chips:%s", c.Type)
+				business.Amount = int(c.Amount * 100) // to cents
+				business.Currency = "USD"
+				business.CartType = c.Provider
+				business.TransactionNumber = c.Number
+				server.SendEvent(business)
 			default:
 				log.Printf("[WARN] Unknown event type %s (%v)", e.Type, e)
 			}
